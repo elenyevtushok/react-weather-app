df6c36c35d97e0061969e0c1cf890020e5c2235a
elenyevtushok <60004031+elenyevtushok@users.noreply.github.com>
2022-11-18 11:36:39 +0100

    Added link to GitHub

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ export default function App() {
   return (
     <div className="App">
 		Weather App
+		<footer>
+			<p>
+				  <a href='https://github.com/elenyevtushok/react-weather-app' rel="noreferrer" target="_blank"
+				>Open-source code</a>
+			</p>
+			<p>Built by Olena Yevtushok</p>
+		</footer>
     </div>
   );
 }
