1ad0735095938583e222d1f04ab2679f6e03d583
elenyevtushok <60004031+elenyevtushok@users.noreply.github.com>
2022-11-21 11:39:55 +0100

    Added Bootstarp

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-		Weather App
-		<footer>
-			<p>
-				  <a href='https://github.com/elenyevtushok/react-weather-app' rel="noreferrer" target="_blank"
-				>Open-source code</a>
-			</p>
-			<p>Built by Olena Yevtushok</p>
-		</footer>
+		<div className='container'>
+			<h1>Weather App</h1>
+			<footer>
+				<p>
+					<a href='https://github.com/elenyevtushok/react-weather-app' rel="noreferrer" target="_blank"
+					>Open-source code</a>
+				</p>
+				<p>Built by Olena Yevtushok</p>
+			</footer>
+		</div>
     </div>
   );
 }
