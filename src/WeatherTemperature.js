b4a2169236ed7a5dfa4b80ce17894aed30d42da4
elenyevtushok <60004031+elenyevtushok@users.noreply.github.com>
2022-11-25 09:54:23 +0100

    Removed conversion to Fahrenheit, made the forecast update working

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,37 +1,11 @@
 import React, { useState } from "react";
 
 export default function WeatherTemperature(props){
-	const [unit, setUnit] = useState("celsius");
-
-	function showFahrenheit(event){
-		event.preventDefault();
-		setUnit("fahrenheit");
-	}
-	function showCelsius(event) {
-		event.preventDefault();
-		setUnit("celsius");
-	}
-	function fahrenheit(){
-		return (props.celsius*9)/5 + 32;
-	}
-	if (unit === "celsius"){
 		return(
 			<div className="WeatherTemperature">
 				<span className="today-weather-temperature ">{Math.round(props.celsius)}</span>	
-				<span className="today-weather-unit">°C |
-					<a href="/" onClick={showFahrenheit}>°F</a>
+				<span className="today-weather-unit">°C
 				</span>
 			</div>
 		);
-	} else {
-		return (
-			<div className="WeatherTemperature">
-				<span className="today-weather-temperature ">{Math.round(fahrenheit())}</span>
-				<span className="today-weather-unit">
-					<a href="/" onClick={showCelsius}>°C</a> 
-					|°F
-				</span>
-			</div>
-		);
-	}
 }
